e279f623f13c9a6f497db10f811fc55ed61ceae4
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-24 14:39:16 +0600

    added the services component for home page

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -5,12 +5,12 @@ const About = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
                 <div className='lg:w-1/2 relative'>
-                    <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
-                    <img src={parts} className="w-2/3 absolute rounded-lg shadow-2xl right-8 top-1/2 border-8 border-white" />
+                    <img src={person} className="w-full lg:w-3/4 rounded-lg shadow-2xl" />
+                    <img src={parts} className="w-full lg:w-2/3 lg:absolute rounded-lg shadow-2xl right-8 top-1/2 border-8 border-white" />
                 </div>
                 <div className='lg:w-1/2 p-4 space-y-5'>
                     <p className='text-xl text-[#FF3811] font-bold'>About Us</p>
-                    <h1 className="text-5xl font-bold w-[400px]">We are qualified & of experience in this field</h1>
+                    <h1 className="text-3xl lg:text-5xl font-bold lg:w-[400px]">We are qualified & of experience in this field</h1>
                     <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don not look even slightly believable. </p>
                     <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don not look even slightly believable. </p>
                     <button className="btn bg-[#FF3811] normal-case text-white">Get More Info</button>
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import Service from "./Service";
+
+const Services = () => {
+    const [services,setServices]=useState([]);
+
+    useEffect(()=>{
+        fetch('/services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+    return (
+        <div>
+            <div className="text-center space-y-5 mt-4">
+                <h3 className="text-3xl text-[#FF3811]">Services</h3>
+                <h1 className="text-6xl font-bold">Our Service Area</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-8">
+                {
+                    services.map(service => <Service
+                    key={service._id}
+                    service={service}
+                    ></Service>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
