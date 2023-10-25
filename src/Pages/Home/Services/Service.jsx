26b2e3cc37a6ecce7bb6b3112eb243c9086047eb
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-26 00:15:08 +0600

    Completed fecthing data from mongoDB database

--- src/Pages/Home/Services/Service.jsx
+++ src/Pages/Home/Services/Service.jsx
@@ -1,6 +1,7 @@
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 const Service = ({ service }) => {
-    const { title, img, price } = service;
+    const { title, img, price, _id } = service;
     return (
         <div className="card bg-base-100 shadow-xl">
             <figure><img className="h-[300px]" src={img} alt="Shoes" /></figure>
@@ -8,7 +9,9 @@ const Service = ({ service }) => {
                 <h2 className="card-title">{title}</h2>
                 <p className="text-[#FF3811] font-bold">Price: ${price}</p>
                 <div className="card-actions justify-end">
-                    <button className="text-xl text-[#FF3811]"><AiOutlineArrowRight></AiOutlineArrowRight></button>
+                    <Link to={`/checkout/${_id}`}>
+                        <button className="btn text-lg normal-case text-[#FF3811] flex items-center gap-2">Book Now:<AiOutlineArrowRight></AiOutlineArrowRight></button>
+                    </Link>
                 </div>
             </div>
         </div>
